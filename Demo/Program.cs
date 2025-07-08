9a9b0a3a41f9d360700db056361fa6ae1618d1d8
Ahmed-Hany2 <ahmedhanypro2@gmail.com>
2025-07-08 21:28:40 +0300

    Jagged Array

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -85,6 +85,17 @@
 
             #endregion
 
+            #region Jagged Array
+            int[][] jaggedArray = new int[4][]
+            {
+                [ 1, 2, 3 ],
+                [4, 5, 6, 7, 8],
+                [9 ],
+                [2, 4, 7, 7]
+            };
+            jaggedArray[0] = [ 1, 2, 3 , 5, 7];
+            #endregion
+
 
 
 
