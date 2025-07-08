9992ffd875a991ab15c55a80b88bf20fc39cf6e3
Ahmed-Hany2 <ahmedhanypro2@gmail.com>
2025-07-08 20:52:02 +0300

    Array Creation Ways

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -32,6 +32,16 @@
             }
             #endregion
 
+            #region Array Creation Ways
+            int[] numbers01 = new int[3];
+            int[] numbers02 = new int[3] { 1, 2, 3};
+            int[] numbers03 = new int[] { 1, 2, 3, 4, 5};
+            int[] numbers04 = { 1, 2, 3, 4, 5 };
+
+            #endregion
+
+
+
 
         }
     }
