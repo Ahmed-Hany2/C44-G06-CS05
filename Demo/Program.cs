afe59ca4f9d68ab306bf3cbaed88289f29af5c04
Ahmed-Hany2 <ahmedhanypro2@gmail.com>
2025-07-08 20:48:29 +0300

    One Dimensional Array

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region One Dimensional Array
+            int[] numbers;
+            numbers = new int[3];
+            // [0, 0 ,0] Default Values
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+            numbers[0] = 10;    
+            numbers[1] = 20;
+            numbers[2] = 30;
+            // [10, 20, 30]
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+            Console.WriteLine($"Size of array => {numbers.Length}, Dimensions of array => {numbers.Rank}");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+            foreach (var item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+            #endregion
+
+
         }
     }
 }
